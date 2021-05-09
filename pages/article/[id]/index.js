4b25d600d3467027620b643388bc432cb7a8bf3b
Annie Huang <annie.hn@gmail.com>
2021-05-09 17:42:10 +1000

    Pass the article as a prop into the page component and display content in /pages/article/[id]/index.js

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,10 +1,17 @@
 import { useRouter } from 'next/router';
 
-const article = () => {
-  const router = useRouter();
-  const { id } = router.query;
+const article = ({ article }) => {
+  // const router = useRouter();
+  // const { id } = router.query;
+  // return <div>This is an article {id}</div>;
 
-  return <div>This is an article {id}</div>;
+  return (
+    <>
+      <h1>{article.title}</h1>
+      <p>{article.body}</p>
+      <br />
+    </>
+  );
 };
 
 export const getServerSideProps = async (context) => {
