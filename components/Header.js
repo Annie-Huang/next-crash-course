598b4af8153d37d6f932b0e0c1af22e745678ac1
Annie Huang <annie.hn@gmail.com>
2021-05-09 16:24:48 +1000

    Test pass in props into style-in-css and it worked.

--- components/Header.js
+++ components/Header.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import headerStyles from '../styles/Header.module.css';
 
+const localStyle = (x) => (
+  <style jsx>{`
+    .title {
+      color: ${x > 3 ? 'red' : 'blue'};
+    }
+  `}</style>
+);
+
 const Header = () => {
+  const x = 2;
   return (
     <div>
-      <h1 className='title'>
+      <h1 className={headerStyles.title}>
         <span>WebDev</span> News
       </h1>
-      <style jsx>{`
-        .title {
-          color: red;
-        }
-      `}</style>
+      <p className={headerStyles.description}>
+        Keep up to date with the latest web
+      </p>
+      {/*{localStyle(2)}*/}
     </div>
   );
 };
