11a02b37e4981ac214056a7b43f81825645ce8cc
Annie Huang <annie.hn@gmail.com>
2021-05-09 21:15:01 +1000

    Add <Meta> into about.js. Will just override the title for Meta component. Will not have duplicate <title> in the head

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,10 @@
+import Meta from '../components/Meta';
+
+// Will just override the title for Meta component. Will not have duplicate <title> in the head.
 const about = () => {
   return (
     <div>
+      <Meta title='about' />
       <h1>About</h1>
     </div>
   );
