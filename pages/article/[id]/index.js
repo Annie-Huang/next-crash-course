596fd9a2f70303797e37d2488f10d77594710b0f
Annie Huang <annie.hn@gmail.com>
2021-05-09 20:59:20 +1000

    Getting the specifc articles from the data from the /api/articles folder

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { server } from '../../../config';
 
 // Data file that it makes the call underneath is: http://localhost:3000/_next/data/development/article/1.json
 const article = ({ article }) => {
@@ -33,6 +34,7 @@ const article = ({ article }) => {
 //     },
 //   };
 // };
+/*
 
 // This will get things from the params.id when on request.
 export const getStaticProps = async (context) => {
@@ -53,6 +55,38 @@ export const getStaticProps = async (context) => {
 // You can go directly http://localhost:3000/article/20 to fetch the data.
 export const getStaticPaths = async () => {
   const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+
+  const articles = await res.json();
+  const ids = articles.map((article) => article.id);
+  const paths = ids.map((id) => ({ params: { id: id.toString() } }));
+
+  console.log('ids=', ids);
+  console.log('paths=', paths);
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+*/
+
+// This will get things from the params.id when on request.
+export const getStaticProps = async (context) => {
+  const res = await fetch(`${server}/api/articles/${context.params.id}`);
+
+  const article = await res.json();
+
+  return {
+    props: {
+      article,
+    },
+  };
+};
+
+// This will get every post from the /posts URL.
+// You can go directly http://localhost:3000/article/20 to fetch the data.
+export const getStaticPaths = async () => {
+  const res = await fetch(`${server}/api/articles`);
 
   const articles = await res.json();
   const ids = articles.map((article) => article.id);
