0ce2d807b4ea000c1b4666ebb63b59e3a7492f86
Annie Huang <annie.hn@gmail.com>
2021-05-09 17:35:15 +1000

    Show how to getServerSideProps (dynamic loading the data when the particular page is loaded)

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -7,4 +7,18 @@ const article = () => {
   return <div>This is an article {id}</div>;
 };
 
+export const getServerSideProps = async (context) => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+  );
+
+  const article = await res.json();
+
+  return {
+    props: {
+      article,
+    },
+  };
+};
+
 export default article;
