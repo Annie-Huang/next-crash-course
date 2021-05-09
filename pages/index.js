3edaa76268f61948474a31a4ae9acd451975d6a4
Annie Huang <annie.hn@gmail.com>
2021-05-09 20:54:13 +1000

    Change our C:\react\next-crash-course\pages\index.js to fretch information from `${server}/api/articles`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import ArticleList from '../components/ArticleList';
+import { server } from '../config';
 
 export default function Home({ articles }) {
   console.log('articles=a', articles);
@@ -15,10 +16,22 @@ export default function Home({ articles }) {
   );
 }
 
-export const getStaticProps = async () => {
+/*export const getStaticProps = async () => {
   const res = await fetch(
     `https://jsonplaceholder.typicode.com/posts?_limit=6`
   );
+  const articles = await res.json();
+
+  return {
+    props: {
+      articles,
+    },
+  };
+};*/
+
+export const getStaticProps = async () => {
+  // Need to be absolute path
+  const res = await fetch(`${server}/api/articles`);
   const articles = await res.json();
 
   return {
