af0dcdb5eec5341a741dd1a9155c18739b803e2b
Annie Huang <annie.hn@gmail.com>
2021-05-09 14:43:05 +1000

    Change Home.module.css to Layout.module.css. Create Layout component and use it to wrap <Comp.jsonent> in MyApp

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Layout from "../components/Layout";
 
+// The <Component> here are the page component
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
