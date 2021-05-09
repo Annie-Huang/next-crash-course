fcd1b0e4f101efa7114e6df50173d2eb3c8c0c3b
Annie Huang <annie.hn@gmail.com>
2021-05-09 17:29:24 +1000

    Show how to get the router.query of id in a component file.

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import { useRouter } from 'next/router';
 
 const article = () => {
-  return <div>This is an article</div>;
+  const router = useRouter();
+  const { id } = router.query;
+
+  return <div>This is an article {id}</div>;
 };
 
 export default article;
